cd96dac29cf64effbcda170bf458a35348d39722
Marco Miralles <marcomiralles@Marcos-MacBook-Air.local>
2023-07-23 17:03:11 +0900

    swapped winding triangle drawing

--- main.cpp
+++ main.cpp
@@ -633,8 +633,8 @@ int main()
     VkClearValue clearValue = {.color = clearColor};
 
     const std::vector<Vertex> vertices = {
-        {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
         {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+        {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
         {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
 
     VkBuffer vertexBuffer;
