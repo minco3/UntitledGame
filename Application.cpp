ad3410d24f593f687e401595dba9ccdebe2e89c2
Marco Miralles <marco.miralles@gmail.com>
2023-10-03 03:06:30 -0700

    removed unused includes in application

--- Application.cpp
+++ Application.cpp
@@ -1,29 +1,12 @@
 #include "Application.hpp"
-#include <stdexcept>
-
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_vulkan.h>
-#include <array>
-#include <filesystem>
-#include <fmt/format.h>
-#include <fmt/ostream.h>
-#include <fstream>
-#include <glm/glm.hpp>
-#include <iomanip>
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <vulkan/vk_enum_string_helper.h>
-#include <vulkan/vulkan.h>
-#include <vulkan/vulkan_beta.h>
 
-Application::Application() :m_Running(true){}
+Application::Application() : m_Running(true) {}
 
 void Application::Run() {}
 void Application::Update()
 {
-while (m_Running)
+    while (m_Running)
     {
         SDL_Event event;
         while (SDL_PollEvent(&event))
@@ -39,4 +22,5 @@ while (m_Running)
                 }
             }
         }
+    }
 }
