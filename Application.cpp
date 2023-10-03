3cc1626b78ea6889b251f639db5c3598dfa5b1cc
Marco Miralles <marco.miralles@gmail.com>
2023-10-03 03:08:07 -0700

    restructured application

--- Application.cpp
+++ Application.cpp
@@ -1,27 +1,31 @@
 #include "Application.hpp"
 #include <SDL2/SDL.h>
 
-Application::Application() : m_Running(true) {}
+Application::Application() {}
 
 void Application::Run()
 {
+    m_Running = true;
     while (m_Running)
     {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_WINDOWEVENT:
-                switch (event.window.event)
-                {
-                case SDL_WINDOWEVENT_CLOSE:
-                    m_Running = false;
-                    break;
-                }
-            }
-        }
+        Update();
         m_Video.Render();
     }
 }
-void Application::Update() {}
+void Application::Update()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_WINDOWEVENT:
+            switch (event.window.event)
+            {
+            case SDL_WINDOWEVENT_CLOSE:
+                m_Running = false;
+                break;
+            }
+        }
+    }
+}
