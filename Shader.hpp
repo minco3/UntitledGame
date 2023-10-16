ee4ec7488247744a051f9388c3705fc4354a563f
Marco Miralles <marco.miralles@gmail.com>
2023-10-15 23:58:38 -0700

    shader class using vulkanhpp

--- Shader.hpp
+++ Shader.hpp
@@ -11,17 +11,13 @@ struct Shader
         vk::raii::Device& device, const std::string& _name,
         const vk::ShaderModuleCreateInfo& vertShaderCreateInfo,
         const vk::ShaderModuleCreateInfo& fragShaderCreateInfo)
-        : name(_name)
+        : name(_name), vertShaderModule(device, vertShaderCreateInfo),
+          fragShaderModule(device, fragShaderCreateInfo)
     {
-        m_Device.CreateShaderModule(
-            m_Device, &vertShaderCreateInfo, nullptr, &vertShaderModule);
-            result);
-        m_Device.CreateShaderModule(
-            m_Device, &fragShaderCreateInfo, nullptr, &fragShaderModule);
     }
     std::string name;
-    vk::ShaderModule vertShaderModule;
-    vk::ShaderModule fragShaderModule;
+    vk::raii::ShaderModule vertShaderModule;
+    vk::raii::ShaderModule fragShaderModule;
 };
 
 std::vector<Shader> LoadShaders(vk::raii::Device& device);
