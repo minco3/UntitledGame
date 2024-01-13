4c842cf7dc43d8d1f9acd9ad2e02dc6e4426f3d4
Marco Miralles <marco.miralles@gmail.com>
2024-01-13 15:27:49 -0800

    fixed misc errors

--- Shader.cpp
+++ Shader.cpp
@@ -20,7 +20,7 @@ std::vector<Shader> LoadShaders(vk::raii::Device& device)
          std::filesystem::directory_iterator(shader_directory))
     {
         // shaderc::Compiler c;
-        if (entry.path().extension() == ".spv") // a.frag(.spv)
+        if (entry.path().extension().string() == ".spv") // a.frag(.spv)
         {
             std::string name =
                 entry.path().stem().stem().string(); // (a).frag.spv
@@ -40,7 +40,7 @@ std::vector<Shader> LoadShaders(vk::raii::Device& device)
             vertShaderPath.append(name + ".vert.spv");
             std::filesystem::path fragShaderPath = shader_directory;
             fragShaderPath.append(name + ".frag.spv");
-            if (shaderType == ".vert")
+            if (shaderType.string() == ".vert")
             {
                 if (!std::filesystem::exists(fragShaderPath))
                 {
@@ -49,7 +49,7 @@ std::vector<Shader> LoadShaders(vk::raii::Device& device)
                     continue;
                 }
             }
-            else if (shaderType == ".frag")
+            else if (shaderType.string() == ".frag")
             {
                 if (!std::filesystem::exists(vertShaderPath))
                 {
