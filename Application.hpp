c754b9721766919b75be7f247b18590f8f5f0726
Marco Miralles <marco.miralles@gmail.com>
2024-01-17 02:30:59 -0800

    remove useless variable

--- Application.hpp
+++ Application.hpp
@@ -16,7 +16,6 @@ public:
 private:
     Video m_Video;
     bool m_Running;
-    bool m_CaptureMouse;
     Camera m_Camera;
     DirectoryWatcher m_DirectoryWatcher;
     std::chrono::steady_clock::time_point m_LastTimePoint;
